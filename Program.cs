b7b4ca33f18c21c456d361b9da05f43e33c8e096
SukranSirinov <100930440+SukranSirinov@users.noreply.github.com>
2022-03-16 11:58:40 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user_2 = new User("Sukran");
+            user_2.Password = "12Shukrans";
+
+            User user_3 = new User("Fuad");
+            user_3.Password = "12Fuadparol";
+
+            string Username;
+            string Password;
+            do
+            {
+                Console.WriteLine("UserName :");
+              Username = Console.ReadLine();
+                if(HasUpper(Username) && HasLower(Username))
+                {
+                    break;
+                }
+            } while (true);
+            do
+            {
+                Console.WriteLine("Password :");
+                Password = Console.ReadLine();
+                if (HasDigit(Password) && HasUpper(Password) && HasLower(Password))
+                {
+                    break ;
+                }
+            } while (true);
+            User user_1 = new User(Username);
+            user_1.Password = Password;
+            User[] users = { user_1, user_2, user_3 };
+            foreach (User user in users)
+            {
+                Console.WriteLine(user.GetData());
+            }
+        }
+        string bool Hasdigit(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        string bool HasUpper(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsUpper(text[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        string bool HasLower(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsLower(text[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    internal class User
+    {
+      public User(string name)
+        {
+            this.Name = name;
+        }
+        private string _name;
+        private string _password;
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                if (value.Length > 5 && value.Length < 25) ;
+                this._name = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this._password;
+            }
+            set
+            {
+                if(value.Length > 7 && value.Length < 26) ;
+                this._password = value;
+            }
+        }
+        static bool CheckString(string str)
+        {
+            bool hasDigit = false;
+            bool hasLower = false;
+            bool hasUpper = false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[i]))
+                    hasUpper = true;
+                else if (char.IsLower(str[i]))
+                   hasLower = true;
+                else if(char.IsDigit(str[i]))
+                    hasDigit = true;
+                if(hasDigit && hasLower && hasUpper)
+                    return true;
+            }
+            return false;
+        }
+            
+    }
+}
